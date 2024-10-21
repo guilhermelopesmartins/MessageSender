9ba9233284f38c67808758b935435f720d030f82
guilhermelopesmartins <guirockmartins@hotmail.com>
2024-10-21 15:33:34 +0200

    send mensages instantly

--- MessageDeliver.py
+++ MessageDeliver.py
@@ -1,6 +1,5 @@
 
 import pywhatkit
-from datetime import datetime, timedelta
 
 def send_messages(content):
     # idea to send a message to the person who fill the form passing some instructions
@@ -8,17 +7,13 @@ def send_messages(content):
         message = 'Hello, I am sick today \nName: {0}\nRoom: {1} \nHouse: {2}\nAppointment: {3} \nHelp: {4}'.format(content['name'], content['room'], content['house'], content['appointment_situation'], content['help'])
         waiting_time_to_send = 15
         close_tab = True
-        waiting_time_to_close = 5
+        waiting_time_to_close = 3
 
         with open('contacts.txt', 'r') as arquivo:    
             contacts = [linha.strip() for linha in arquivo.readlines()]
 
         for contact in contacts:
-            current_time = datetime.now()
-            new_time = current_time + timedelta(minutes=1)
-            time_hour = new_time.hour
-            time_minute = new_time.minute
-            pywhatkit.sendwhatmsg(contact, message, time_hour, time_minute, waiting_time_to_send, close_tab, waiting_time_to_close)
+            pywhatkit.sendwhatmsg_instantly(contact, message, waiting_time_to_send, close_tab, waiting_time_to_close)
         
         return 'Messages delivered succefully'
     except:
